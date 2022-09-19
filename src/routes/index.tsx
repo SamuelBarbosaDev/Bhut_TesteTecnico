f727e603108d58077d77625f3fc9fa5988a27810
SamuelBarbosaDev <samueloficial@protonmail.com>
2022-09-19 12:02:09 -0300

    pequena alteração

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,10 +7,10 @@ export const AppRoutes = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/bhut-react" element={<Home/>}/>
       <Route path="/details/:id" element={<Details/>}/>
 
-      <Route path="*" element={<Navigate to="/" />} />
+      <Route path="*" element={<Navigate to="/bhut-react" />} />
     </Routes>
   );
 };
